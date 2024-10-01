7924069b64b48ed0fa648ba0629230c5074412ca
taohidkhan22 <taohid130@gmail.com>
2024-10-01 13:43:30 +0600

    Example of block scope of let const and var

---
+++ JAVASCRIPT/Block Scope and Shadow/script.js
@@ -0,0 +1,16 @@
+//Curly braces define the block
+// when multiple statement in the js needs to combine together then the term block comes up
+// Let and scope are block scope because 
+
+{
+  let a = 10;
+  var  b = 20;
+   const c = 30;
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+console.log(b);
+console.log(a);
+
+console.log(c);
