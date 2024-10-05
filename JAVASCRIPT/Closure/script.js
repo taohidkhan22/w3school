e1767c4484d83cce1aa95ea279254bfbc9b3823b
taohidkhan22 <taohid130@gmail.com>
2024-10-05 16:22:36 +0600

    setTimeout function with closure

--- JAVASCRIPT/Closure/script.js
+++ JAVASCRIPT/Closure/script.js
@@ -1,11 +1,15 @@
-function x() {
-  var a = 39;
+function x(){
 
-  return function y() {
-    console.log(a);
-  };
+    var a=39;
+   
+    return  function y(){
+        console.log(a);
+    }
+    
 }
 
-var z = x();
+
+
+var z= x();
 console.log(z);
 z();
---
+++ JAVASCRIPT/setTimeout/script.js
@@ -0,0 +1,7 @@
+function x(){
+    var a=30;
+    setTimeout(function(){
+        console.log(a);
+    },5000);
+}
+x();
