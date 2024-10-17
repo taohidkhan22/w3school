85a9e23b963022e1192bef963ed6d20779c6ed72
taohidkhan22 <taohid130@gmail.com>
2024-10-17 19:31:33 +0600

    First class func, named function

--- JAVASCRIPT/First Class Function/script.js
+++ JAVASCRIPT/First Class Function/script.js
@@ -1,7 +1,5 @@
 
-a();
-c();//hoisting problem becasue when c invoke it sotred as value undefined
-//Function statement
+//Function statement{hoisting problem becasue when c invoke it sotred as value undefined}
 
 function a(){
     console.log("funciton a");
@@ -9,7 +7,32 @@ function a(){
 
 
 //Function Expression
-var c=function(){
-    console.log("function c with as variable");
+var c= function (){
+    console.log("function `C`");
+    return x;
 }
 
+
+
+//Named function expression
+
+var d= function abc(){
+    console.log(abc);
+}
+
+d();
+
+//First class Function
+
+let s= function xxx(param){
+    console.log('First class function');
+    console.log(param);
+    return xxx;
+   
+}
+
+
+const p=s;
+
+p(d);
+console.log(p);
